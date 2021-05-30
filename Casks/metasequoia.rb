208aa588c133d5c8fcb9c9e79e9aaae4b346019f
anatawa12 <anatawa12@icloud.com>
2021-05-30 13:40:39 +0900

    upgrade metasequoia to 4.7.7b

--- Casks/metasequoia.rb
+++ Casks/metasequoia.rb
@@ -1,6 +1,6 @@
 cask 'metasequoia' do
-  version "4.7.7"
-  sha256 "2c214b2d0ce88acaccee8502170a21f26d729a6755a5654f09e49ec04bcaff28"
+  version "4.7.7b"
+  sha256 "c83815dc50ca4c57828ecdcffe0351a01dc59d99ad69d35f21a8739e7530f502"
 
   url "https://www.metaseq.net/metaseq/Metasequoia-#{version}-Installer.dmg"
   appcast 'https://www.metaseq.net/en/download.html'
